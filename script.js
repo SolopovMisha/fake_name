7b42b00ba2f53051be862b719c777efab702d8aa
Bezdar <100799406+SolopovMisha@users.noreply.github.com>
2025-11-28 19:17:39 +0400

    update

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-let firstName = "Иван"
-let lastName = "Иванов"
-let surname = "Иванович"
-console.log(lastName)
-console.log(firstName)
-console.log(surname)
-console.log(lastName, firstName, surname)
-
-
 const selectDay = document.getElementById('day');
 const selectMonth = document.getElementById('month');
 const selectYear = document.getElementById('year');
-
 const resultDiv = document.getElementById('result'); 
 
 
 function updateResult() {
-
   const dayValue = selectDay ? selectDay.value : 'N/A';
   const monthValue = selectMonth ? selectMonth.value : 'N/A';
   const yearValue = selectYear ? selectYear.value : 'N/A';
@@ -23,18 +12,14 @@ function updateResult() {
   const fullResult = `${dayValue} ${monthValue} ${yearValue}`;
   if (resultDiv) {
       resultDiv.innerHTML = fullResult;
-  }
-  
+  } 
   console.log(fullResult);
 }
 
 
-
 function handleChange(event) {
-
-  updateResult();
+updateResult();
 }
-
 
 
 if (selectDay) {
@@ -46,6 +31,5 @@ if (selectMonth) {
 if (selectYear) {
     selectYear.addEventListener('change', handleChange);
 }
-
 
 updateResult();
